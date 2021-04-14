ba17026e5bf76d237190e6155703430f164d6760
ihwang <sheep9121@gmail.com>
2021-04-14 23:54:23 +0300

    stop sharing key

--- src/build.py
+++ src/build.py
@@ -1,10 +1,5 @@
 import os
 import sys
 
-def build_image(config):
-    # iso_pathname = config["ISO_PATHNAME"].replace("\"", "")
-    iso_pathname = config["ISO_PATHNAME"].strip()
-    if os.access(iso_pathname, os.F_OK):
-        print("roger_skyline1: \"" + iso_pathname + "\" file has already been created", file=sys.stderr)
-        exit(1)
+def build_image():
     os.system("cd ./iso && bash gen_preseed_iso.sh")
